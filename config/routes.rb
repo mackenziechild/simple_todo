f9e2af228b32ba758e80a15f4fe7a864f1af2110
Mackenzie Child <mackenzie.child@gmail.com>
2014-03-26 16:52:54 -0600

    Generate todo list scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Todo::Application.routes.draw do
+  resources :todo_lists
+
+  root 'todo_lists#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
