0dd90bf63c9b3d47f22bf1fe1208cc8e85d0384c
Mackenzie Child <mackenzie.child@gmail.com>
2014-03-27 10:14:56 -0600

    Add todo item model and associated specs

--- Gemfile
+++ Gemfile
@@ -20,4 +20,5 @@ end
 
 group :test do
 	gem 'capybara', '~> 2.1.0'
+	gem 'shoulda-matchers', '~> 2.4.0'
 end
--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,4 +1,6 @@
 class TodoList < ActiveRecord::Base
+	has_many :todo_items
+
 	validates :title, presence: true
 	validates :title, length: { minimum: 3}
 	validates :description, presence: true
