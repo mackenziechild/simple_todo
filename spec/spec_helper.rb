21d63a46064b0ad74b84bd655770e5a483554e37
Mackenzie Child <mackenzie.child@gmail.com>
2014-03-28 15:43:19 -0600

    add rails dom ID helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,8 @@ ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
   config.include TodoListHelpers, type: :feature
+  config.include RailsDomIdHelper, type: :feature
+
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
