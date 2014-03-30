66f72e8a6c33ad39ab237835f4589a0d4edde718
Mackenzie Child <mackenzie.child@gmail.com>
2014-03-30 11:13:02 -0600

    Clean up todo mark completion view

--- spec/features/todo_items/complete_spec.rb
+++ spec/features/todo_items/complete_spec.rb
@@ -13,4 +13,22 @@ describe "Completing todo items" do
 		todo_item.reload
 		expect(todo_item.completed_at).to_not be_nil
 	end
+
+	context "with completed items" do
+		let!(:completed_todo_item) { todo_list.todo_items.create(content: "Eggs", completed_at: 5.minutes.ago) }
+
+		it "shows completed items as complete" do
+			visit_todo_list todo_list
+			within dom_id_for(completed_todo_item) do
+				expect(page).to have_content(completed_todo_item.completed_at)
+			end
+		end
+
+		it "does not give the option to mark complete" do
+			visit_todo_list todo_list
+			within dom_id_for(completed_todo_item) do
+				expect(page).to_not have_content("Mark Complete")
+			end
+		end
+	end
 end
--- spec/models/todo_item_spec.rb
+++ spec/models/todo_item_spec.rb
@@ -2,4 +2,18 @@ require 'spec_helper'
 
 describe TodoItem do
   it { should belong_to(:todo_list) }
+
+  describe "#completed?" do
+  	let(:todo_item) { TodoItem.create(content: "Hello") }
+
+  	it "is false when completed_at is blank" do
+  		todo_item.completed_at = nil
+  		expect(todo_item.completed?).to be_false
+  	end
+
+  	it "returns true when completed_at is not empty" do
+  		todo_item.completed_at = Time.now
+  		expect(todo_item.completed?).to be_true
+  	end
+  end
 end
