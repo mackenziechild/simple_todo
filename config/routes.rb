ad2a81c77cad755c613dc2bfd773c5df2ef94e5b
Mackenzie Child <mackenzie.child@gmail.com>
2014-03-30 10:48:23 -0600

    Mark todo item as complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Todo::Application.routes.draw do
   resources :todo_lists do
-    resources :todo_items
+    resources :todo_items do
+      member do
+        patch :complete
+      end
+    end
   end
 
   root 'todo_lists#index'
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140327160735) do
+ActiveRecord::Schema.define(version: 20140330163637) do
 
   create_table "todo_items", force: true do |t|
     t.integer  "todo_list_id"
     t.string   "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "completed_at"
   end
 
   add_index "todo_items", ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
